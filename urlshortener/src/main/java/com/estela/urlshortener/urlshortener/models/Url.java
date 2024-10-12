cdb5671a595b4329755a591ee34d0c23a972dd1d
Estela de Lacerda Oliveira <elo@cesar.school>
2024-10-11 23:44:58 -0300

    add: Innitializing CRUD and creating  Url class.

---
+++ urlshortener/src/main/java/com/estela/urlshortener/urlshortener/models/Url.java
@@ -0,0 +1,78 @@
+package com.estela.urlshortener.urlshortener.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "url")
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "originalUrl", nullable = false, columnDefinition = "TEXT")
+    private String originalUrl;
+
+    @Column(name = "shortenedUrl", nullable = false, length = 20)
+    private String shortenedUrl;
+
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "accessCount")
+    private int accessCount;
+
+    // Construtores
+    public Url() {
+        // Construtor vazio para o JPA
+    }
+
+    public Url(String originalUrl, String shortenedUrl) {
+        this.originalUrl = originalUrl;
+        this.shortenedUrl = shortenedUrl;
+        this.createdAt = LocalDateTime.now();
+        this.accessCount = 0;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortenedUrl() {
+        return shortenedUrl;
+    }
+
+    public void setShortenedUrl(String shortenedUrl) {
+        this.shortenedUrl = shortenedUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getAccessCount() {
+        return accessCount;
+    }
+
+    public void setAccessCount(int accessCount) {
+        this.accessCount = accessCount;
+    }
+
+}

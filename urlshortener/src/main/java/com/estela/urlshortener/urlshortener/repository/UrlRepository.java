acaf62eee28427d85affa7c86f9b4e1830afd933
Estela de Lacerda Oliveira <elo@cesar.school>
2024-10-13 20:35:37 -0300

    add: Creating service methods

--- urlshortener/src/main/java/com/estela/urlshortener/urlshortener/repository/UrlRepository.java
+++ urlshortener/src/main/java/com/estela/urlshortener/urlshortener/repository/UrlRepository.java
@@ -1,9 +1,11 @@
 package com.estela.urlshortener.urlshortener.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.estela.urlshortener.urlshortener.models.Url;
 
+@Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
-    
+    // Método para encontrar a URL original pela URL encurtada
+    Url findByShortenedUrl(String shortenedUrl);
 }
--- urlshortener/src/main/java/com/estela/urlshortener/urlshortener/services/UrlService.java
+++ urlshortener/src/main/java/com/estela/urlshortener/urlshortener/services/UrlService.java
@@ -1,15 +1,18 @@
 package com.estela.urlshortener.urlshortener.services;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+
 import com.estela.urlshortener.urlshortener.models.Url;
 import com.estela.urlshortener.urlshortener.repository.UrlRepository;
 
 public class UrlService {
-    
+
     private UrlRepository urlRepository;
 
     public void insertUrl(Url newUrl) {
-        urlRepository.save(newUrl); 
+        urlRepository.save(newUrl);
     }
 
     public List<Url> getAllUrl() {
@@ -18,15 +21,81 @@ public class UrlService {
 
     public void updateUrl(long id, Url updatedUrl) {
         Url existingUrl = urlRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("URL não encontrada com o ID: " + id));
-        
+                .orElseThrow(() -> new RuntimeException("URL de ID: " + id + " não encontrada."));
+
         existingUrl.setOriginalUrl(updatedUrl.getOriginalUrl());
         existingUrl.setShortenedUrl(updatedUrl.getShortenedUrl());
-        urlRepository.save(existingUrl); 
+        urlRepository.save(existingUrl);
     }
 
-    public void deleteUrl(long id){
-        urlRepository.deleteById(id); 
+    public void deleteUrl(long id) {
+        urlRepository.deleteById(id);
     }
 
+    public String registerUrl(String originalUrl) {
+        Url url = new Url();
+        url.setOriginalUrl(originalUrl);
+
+        url = urlRepository.save(url);
+
+        String shortenedUrl = encodeBase62(url.getId());
+
+        url.setShortenedUrl(shortenedUrl);
+        urlRepository.save(url);
+
+        return shortenedUrl;
+    }
+
+    public String accessShortenedUrl(String shortenedUrl) {
+        try {
+            Url url = urlRepository.findByShortenedUrl(shortenedUrl);
+
+            if (url != null) {
+                url.setAccessCount(url.getAccessCount() + 1);
+                urlRepository.save(url);
+
+                return url.getOriginalUrl();
+            } else {
+                throw new Exception("URL encurtada não encontrada.");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao acessar a URL: " + e.getMessage());
+            return "URL não encontrada."; 
+        }
+    }
+
+    public Url visualizarEstatisticas(String shortenedUrl) {
+        try {
+            Url url = urlRepository.findByShortenedUrl(shortenedUrl);
+
+            if (url != null) {
+                int totalAccess = url.getAccessCount();
+
+                long daysSinceCreation = ChronoUnit.DAYS.between(url.getCreatedAt(), LocalDate.now());
+                double avgAccessDay = (daysSinceCreation > 0) ? (double) totalAccess / daysSinceCreation
+                        : totalAccess;
+
+                System.out.println("Número total de Acessos: " + totalAccess);
+                System.out.println("Média de Acessos por Dia: " + avgAccessDay);
+
+                return url; 
+            } else {
+                throw new Exception("URL encurtada não encontrada.");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao visualizar as estatísticas: " + e.getMessage());
+            return null; 
+        }
+    }
+
+    private String encodeBase62(long id) {
+        String base62Chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder encoded = new StringBuilder();
+        while (id > 0) {
+            int rest = (int) (id % 62);
+            encoded.append(base62Chars.charAt(rest));
+            id /= 62;
+        }
+        return encoded.reverse().toString();
+    }
 }
